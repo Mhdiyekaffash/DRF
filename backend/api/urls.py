e5c3e45103971fe62a173afca7c26c4d3a43b98f
kfash <kfashmhdyh@gmail.com>
2022-03-28 19:41:23 +0430

    Add 4 permissions

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,7 +5,7 @@ app_name = 'api'
 
 urlpatterns = [
     path('', ArticleList.as_view(), name='List'),
-    path('<slug:slug>', ArticleDetail.as_view(), name='Detail'),
+    path('<int:pk>', ArticleDetail.as_view(), name='Detail'),
     path('users/', UserList.as_view(), name='user_list'),
     path('users/<int:pk>', UserDetail.as_view(), name='user_detail'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAdminUser
+from .permissions import IsSuperUser, IsStaffOrReadOnly, IsAuthorOrReadOnly, IsSuperUserOrStaffOrReadOnly
 from blog.models import Article
 from .serializers import ArticleSerializer, UserSerializer
 from django.contrib.auth.models import User
@@ -13,14 +15,17 @@ class ArticleList(ListCreateAPIView):
 class ArticleDetail(RetrieveUpdateDestroyAPIView):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-    lookup_field = 'slug'  # فیلدی که بر اساس اون قراره object ما select بشه . به صورت پیش فرض pk هست.
+    # lookup_field = 'slug'  # فیلدی که بر اساس اون قراره object ما select بشه . به صورت پیش فرض pk هست.
+    permission_classes = (IsStaffOrReadOnly, IsAuthorOrReadOnly)
 
 
 class UserList(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsSuperUserOrStaffOrReadOnly, )
 
 
 class UserDetail(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (IsSuperUserOrStaffOrReadOnly, )
